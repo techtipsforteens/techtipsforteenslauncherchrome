a8c7e8f711ed2accdfaa5e6053a8514b30a91cbf
violinluke <71672636+violinluke@users.noreply.github.com>
2021-01-23 09:17:51 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+document.getElementById("openHome").addEventListener("click", openHome);
+document.getElementById("openArticles").addEventListener("click", openArticles);
+document.getElementById("openForums").addEventListener("click", openForums);
+document.getElementById("openCollections").addEventListener("click", openCollections);
+document.getElementById("openContact").addEventListener("click", openContact);
+
+function openHome() {
+  window.open("https://www.techtipsforteens.com");
+}
+function openArticles() {
+  window.open("https://techtipsforteens.com/?page_id=7");
+}
+function openForums() {
+  window.open("https://techtipsforteens.com/?page_id=86");
+}
+function openCollections() {
+  window.open("https://techtipsforteens.com/?page_id=2");
+}
+function openContact() {
+  window.open("https://techtipsforteens.com/?page_id=81");
+}
